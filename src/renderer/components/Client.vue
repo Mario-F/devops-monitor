b96b5beca956797e61b9f3b5fe5dc71f35c48a2f
Mario-F <github@fritschen.net>
2018-01-28 17:25:46 +0100

    Add getter by connection id

--- src/renderer/components/Client.vue
+++ src/renderer/components/Client.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>Client pane: {{ idClient }}</div>
+  <div>
+    <div>Client pane: {{ idClient }}</div>
+    <div>Connection: {{ connectionById(idClient) }}</div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data() {
     return {
       idClient: this.$route.params.id,
     }
+  },
+  computed: {
+    ...mapGetters(['connectionById'])
   },
   mounted: function() {
     console.log(`Client ${this.idClient} mounted`)
--- src/renderer/store/getters.js
+++ src/renderer/store/getters.js
@@ -2,6 +2,10 @@ export const connections = (state) => {
   return state.connections
 }
 
+export const connectionById = (state) => (id) => {
+  return state.connections.find(c => c._id === id)
+}
+
 export const connectionsEstablished = (state) => {
   return state.connections.filter(c => !!c.established)
 }
