274f52f337732c6cdc86a97dbeba4cf20c61b818
Mario-F <github@fritschen.net>
2018-07-28 09:24:30 +0200

    Add Table for displays

--- src/renderer/components/Client.vue
+++ src/renderer/components/Client.vue
@@ -1,5 +1,38 @@
 <template>
   <div>
+
+    <!-- Displays header / table -->
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Displays</h1>
+    </header>
+    <table class="table-striped">
+      <thead>
+        <tr><th>Host</th><th>Display ID</th><th>Dimensions</th><th>Alive</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="dis in connectionById(idClient).displays">
+          <td>
+            {{ dis.hostname }}
+          </td>
+          <td>
+            {{ dis.id }}
+          </td>
+          <td>
+            {{ dis.size.width }}x{{ dis.size.height }}
+          </td>
+          <td>
+            {{ dis.connected }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Playlist header / table -->
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Playlists</h1>
+    </header>
+
+    <!-- TEMP: DEBUG -->
     <div>Client pane: {{ idClient }}</div>
     <div>Connection: {{ connectionById(idClient) }}</div>
     <div>Displays: {{ connectionById(idClient).displays }}</div>
