be60a48c42c0a6f0ec396798eb0330177b08fc55
Mario-F <github@fritschen.net>
2018-07-28 10:24:58 +0200

    Add active playlist to display table

--- src/renderer/components/Client.vue
+++ src/renderer/components/Client.vue
@@ -1,62 +1,57 @@
 <template>
   <div>
 
-    <!-- Tab overview -->
-    <div class="tab-group">
-      <div class="tab-item" v-bind:class="{ active: tabActive == 'displays' }" v-on:click="tabActive = 'displays'">
-        Displays
-      </div>
-      <div class="tab-item" v-bind:class="{ active: tabActive == 'playlists' }" v-on:click="tabActive = 'playlists'">
-        Playlists
-      </div>
-    </div>
-
     <!-- Displays table -->
-    <template v-if="tabActive == 'displays'">
-      <table class="table-striped">
-        <thead>
-          <tr><th>Host</th><th>Display ID</th><th>Dimensions</th><th>Alive</th></tr>
-        </thead>
-        <tbody>
-          <tr v-for="dis in connectionById(idClient).displays">
-            <td>
-              {{ dis.hostname }}
-            </td>
-            <td>
-              {{ dis.id }}
-            </td>
-            <td>
-              {{ dis.size.width }}x{{ dis.size.height }}
-            </td>
-            <td>
-              {{ dis.connected }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </template>
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Displays</h1>
+    </header>
+    <table class="table-striped">
+      <thead>
+        <tr><th>Host</th><th>Display ID</th><th>Dimensions</th><th>Alive</th><th>Playlist</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="dis in connectionById(idClient).displays">
+          <td>
+            {{ dis.hostname }}
+          </td>
+          <td>
+            {{ dis.id }}
+          </td>
+          <td>
+            {{ dis.size.width }}x{{ dis.size.height }}
+          </td>
+          <td>
+            {{ dis.connected }}
+          </td>
+          <td>
+            {{ playlistByDisplay(dis.id) }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <!-- Playlist table -->
-    <template v-if="tabActive == 'playlists'">
-      <table class="table-striped">
-        <thead>
-          <tr><th>Name</th><th>Display (Active)</th><th>Items</th></tr>
-        </thead>
-        <tbody>
-          <tr v-for="ply in connectionById(idClient).playlists">
-            <td>
-              {{ ply.name }}
-            </td>
-            <td>
-              {{ ply.show }}
-            </td>
-            <td>
-              {{ ply.items.length }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </template>
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Playlists</h1>
+    </header>
+    <table class="table-striped">
+      <thead>
+        <tr><th>Name</th><th>Display (Active)</th><th>Items</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="ply in connectionById(idClient).playlists">
+          <td>
+            {{ ply.name }}
+          </td>
+          <td>
+            {{ ply.show }}
+          </td>
+          <td>
+            {{ ply.items.length }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <!-- TEMP: DEBUG -->
     <header class="toolbar toolbar-header">
@@ -75,12 +70,18 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      tabActive: 'displays',
       idClient: this.$route.params.id,
     }
   },
   computed: {
-    ...mapGetters(['connectionById'])
+    ...mapGetters(['connectionById']),
+    playlistByDisplay: function() {
+      return (idDisplay) => {
+        let resPly = this.connectionById(this.idClient).playlists.filter(p => p.show == idDisplay)
+        if(!resPly) return false
+        return resPly.map(p => p.name).join()
+      }
+    },
   },
   mounted: function() {
     console.log(`Client ${this.idClient} mounted`)
